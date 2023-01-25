255a9a8ab5ae0e4cdccc101d6cce9b0ae2d1d6d4
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-01-25 16:32:14 +0700

    change content (1)

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -8,7 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useEffect } from "react";
-import logo from "../../static/favicon.png";
+// import logo from "../../static/favicon.png";
 import { useLocation } from "react-router-dom";
 import { links } from "./links";
 
@@ -38,7 +38,7 @@ export default function Home({ setSelectedIndex }: Props) {
       <Grid item xs={3}>
         <Stack direction={{ xs: "column", sm: "row-reverse" }} spacing={2}>
           <Box display="flex" sx={{ justifyContent: "center" }}>
-            <img src={logo} width="100px" alt="logo" />
+            {/* <img src={logo} width="100px" alt="logo" /> */}
           </Box>
           <Box>
             <Grid
@@ -52,7 +52,7 @@ export default function Home({ setSelectedIndex }: Props) {
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
               <Typography variant="subtitle1" gutterBottom>
-                Always difficult, always beautiful
+                If we never try, how will we know ?
                 {/* Better an{' '}
                 <Box fontWeight="fontWeightMedium" display="inline">
                   oops
